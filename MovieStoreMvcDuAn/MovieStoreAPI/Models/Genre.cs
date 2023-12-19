8a9bae1a4e842f66af22ff21892ea9ba19de709d
TusCute <editer2k4@gmail.com>
2023-12-19 09:56:33 +0700

    THÊM API Người dùng và Tạo phim

---
+++ MovieStoreMvcDuAn/MovieStoreAPI/Models/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieStoreAPI.Models;
+
+public partial class Genre
+{
+    public int Id { get; set; }
+
+    public string GenreName { get; set; } = null!;
+}
---
+++ MovieStoreMvcDuAn/MovieStoreAPI/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieStoreAPI.Models;
+
+public partial class Movie
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? ReleaseYear { get; set; }
+
+    public string? MovieImage { get; set; }
+
+    public string Cast { get; set; } = null!;
+
+    public string Director { get; set; } = null!;
+}
