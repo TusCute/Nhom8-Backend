cc789608ab5e4eafe3c01351b2d9dd43073b5391
NgocTu2408 <nguyenngoctu9035@gmail.com>
2023-12-21 15:31:38 +0700

    Update Layout

---
+++ MovieStore/MovieStoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MovieStoreMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
